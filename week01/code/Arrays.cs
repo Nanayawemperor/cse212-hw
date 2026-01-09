03084e1d601b6abd104b3ddea7de0917171230ee
Nanayawemperor <emmaokyere095@gmail.com>
2026-01-09 16:56:01 +0000

    week 01

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,11 +9,17 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create an array of type double with the given length.
+        // This array will store all the multiples of the given number. 
+        double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+        // A loop is used to go through each index of the array.
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples;
     }
 
     /// <summary>
@@ -26,8 +32,20 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //Determine how many items will be moved from the end of the list.
+        int splitIndex = data.Count - amount;
+
+        // Get the portion of the list that will move to the front.
+        List<int> rightPart = data.GetRange(0,splitIndex);
+
+        //Get the remaining portion of the list 
+        List<int> LeftPart = data.GetRange(0, splitIndex);
+
+        //Clear the original list.
+        data.Clear();
+
+        //Add the right part to the front 
+        data.AddRange(rightPart);
+        
     }
 }
